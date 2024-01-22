ef9fb60d2075bb0be467c75f13b81c58afcc14cd
Timur Sabirov <roboxtim@gmail.com>
2024-01-22 16:59:11 +0200

    Fixed Album interface

---
+++ src/Theme/theme-wtf.ts
@@ -0,0 +1,5 @@
+export const themeWtf = {
+
+}
+
+export type Theme = typeof themeWtf;
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,12 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { GlobalStyle } from "./Theme/globalStyle.ts";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./Theme/theme.ts";
+import { themeWtf } from "./Theme/theme-wtf.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={themeWtf}>
         <GlobalStyle />
         <App />
       </ThemeProvider>
